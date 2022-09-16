904ea77fdb3dc607e2e400488c736466c9a80acc
Armin <armin.arduino@gmail.com>
2022-09-16 10:57:35 +0200

    Closes #30

---
+++ src/Vocab_AstroBlaster.h
@@ -0,0 +1,48 @@
+/*
+ * Vocab_AstroBlaster.h
+ *
+ *  Created on: 08.01.2019
+ *      Author: Armin
+ */
+
+#ifndef _TALKIE_VOCAB_ASTROBLASTER_H
+#define _TALKIE_VOCAB_ASTROBLASTER_H
+
+#include <Arduino.h>
+
+extern const uint8_t zero[] PROGMEM;
+extern const uint8_t one[] PROGMEM;
+extern const uint8_t two[] PROGMEM;
+extern const uint8_t three[] PROGMEM;
+extern const uint8_t four[] PROGMEM;
+extern const uint8_t five[] PROGMEM;
+extern const uint8_t six[] PROGMEM;
+extern const uint8_t seven[] PROGMEM;
+extern const uint8_t eight[] PROGMEM;
+extern const uint8_t nine[] PROGMEM;
+extern const uint8_t ten[] PROGMEM;
+extern const uint8_t alert[] PROGMEM;
+extern const uint8_t critical[] PROGMEM;
+extern const uint8_t doublepoints[] PROGMEM;
+extern const uint8_t exerciseextremecaution[] PROGMEM;
+extern const uint8_t extrafuel[] PROGMEM;
+extern const uint8_t fighterpilotsneededinsectorwars[] PROGMEM;
+extern const uint8_t fuelstatus[] PROGMEM;
+extern const uint8_t invaderinsector[] PROGMEM;
+extern const uint8_t laser[] PROGMEM;
+extern const uint8_t marginal[] PROGMEM;
+extern const uint8_t operational[] PROGMEM;
+extern const uint8_t outoffuel[] PROGMEM;
+extern const uint8_t overheated[] PROGMEM;
+extern const uint8_t playastroblaster[] PROGMEM;
+extern const uint8_t player[] PROGMEM;
+extern const uint8_t playover[] PROGMEM;
+extern const uint8_t remaining[] PROGMEM;
+extern const uint8_t ship[] PROGMEM;
+extern const uint8_t ships[] PROGMEM;
+extern const uint8_t warpactivated[] PROGMEM;
+extern const uint8_t temperature[] PROGMEM;
+extern const uint8_t tobattlestation[] PROGMEM;
+extern const uint8_t standbyfordockingmanouver[] PROGMEM;
+
+#endif // _TALKIE_VOCAB_ASTROBLASTER_H
